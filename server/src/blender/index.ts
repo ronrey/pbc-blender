df31912e3e07547a7caac8ba7d5b54c1ee3ae75c
ron <ronrey@gmail.com>
2024-07-05 12:39:29 -0700

    changing the qrcode so that it function as a link and as a data source

--- server/src/blender/index.ts
+++ server/src/blender/index.ts
@@ -27,14 +27,45 @@ export class Blender {
 			return { moduleId: i, module: new Module(m) };
 		});
 	}
-	private GetPathByCoffeeId(coffeeId: number): StationPath | undefined {
-		debugger;
-		const active = this.coffeeToModules.filter(coffee => coffee.state === 'active');
-		const coffee: CoffeeModule | undefined = active.find((coffee: CoffeeModule) => coffee.coffeeId === coffeeId);
-		if (!coffee) {
-			throw new Error('Coffee not found');
+	// private GetPathByCoffeeId(coffeeId: number): StationPath | undefined {
+	// 	debugger;
+	// 	const active = this.coffeeToModules.filter(coffee => coffee.state === 'active');
+	// 	const coffee: CoffeeModule | undefined = active.find((coffee: CoffeeModule) => coffee.coffeeId === coffeeId);
+	// 	if (!coffee) {
+	// 		throw new Error('Coffee not found');
+	// 	}
+	// 	return [coffee.moduleId, coffee.stationId];
+	// }
+	private async GetPathByCoffeeId(coffeeId: number): Promise<StationPath | undefined> {
+		const active = this.coffeeToModules.filter(coffee => coffee.state === 'active' && coffee.coffeeId === coffeeId);
+
+		if (active.length === 0) {
+			throw new Error('Coffee not found or not active');
 		}
-		return [coffee.moduleId, coffee.stationId];
+
+		if (active.length === 1) {
+			return [active[0].moduleId, active[0].stationId];
+		}
+
+		// Multiple stations with the same coffee, find the one with the most coffee
+		let maxCoffeeAmount = -1;
+		let selectedStation: CoffeeModule | undefined;
+
+		for (const station of active) {
+			const module = this.modules[station.moduleId].module;
+			const coffeeAmount = await module.getSiloGrams(station.stationId);
+
+			if (coffeeAmount > maxCoffeeAmount) {
+				maxCoffeeAmount = coffeeAmount;
+				selectedStation = station;
+			}
+		}
+
+		if (!selectedStation) {
+			throw new Error('Unable to determine station with most coffee');
+		}
+
+		return [selectedStation.moduleId, selectedStation.stationId];
 	}
 	public async setCoffeeMap(coffeeToModules: CoffeeModule[]): Promise<Status> {
 		this.coffeeToModules = coffeeToModules;
